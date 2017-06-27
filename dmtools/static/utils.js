ad92f81aeed746941be319f21f4167b2af99fbf2
Anthony Caggiano <onaiggaca@gmail.com>
2017-06-27 15:38:05 -0400

    more static content and styling

--- dmtools/static/utils.js
+++ dmtools/static/utils.js
@@ -1,10 +1,73 @@
+if ($('.flash').is(":empty")) {
+    $(".flash").hide();
+}
+
 $(function () {
     $('#party-tab a:first').tab('show')
 })
 
-if ($('.flash').is(":empty")) {
-    $(".flash").hide();
-}
+$(function () {
+  $('.ammunition-property').popover({
+    title: "Ammunition Property",
+    content: "You can use a weapon that has the ammunition property to make a ranged attack only if you have ammunition to fire from the weapon. Each time you attack with the weapon, you expend one piece of ammunition. Drawing the ammunition from a quiver, case, or other container is part of the attack. At the end of the battle, you can recover half your expended ammunition by taking a minute to search the battlefield."
+  })
+})
+
+$(function () {
+  $('.finesse-property').popover({
+    title: "Finesse Property",
+    content: "When making an attack with a finesse weapon, you use your choice o f your Strength or Dexterity modifier for the attack and damage rolls. You must use the same modifier for both rolls. Heavy. Small creatures have disadvantage on attack rolls with heavy weapons. A heavy weapon's size and bulk make it too large for a Small creature to use effectively."
+  })
+})
+
+$(function () {
+  $('.light-property').popover({
+    title: "Light Property",
+    content: "A light weapon is small and easy to handle, making it ideal for use when fighting with two weapons. See the rules for two-weapon fighting in chapter 9. Loading. Because of the time required to load this weapon, you can fire only one piece of ammunition from it when you use an action, bonus action, or reaction to fire it, regardless of the number of attacks you can normally make."
+  })
+})
+
+$(function () {
+  $('.range-property').popover({
+    title: "Range Property",
+    content: "A weapon that can be used to make a ranged attack has a range shown in parentheses after the ammunition or thrown property. The range lists two numbers. The first is the weapon's normal range in feet, and the second indicates the weapon's maximum range. When attacking a target beyond normal range, you have disadvantage on the attack roll. You can't attack a target beyond the weapon's long range."
+  })
+})
+
+$(function () {
+  $('.reach-property').popover({
+    title: "Reach Property",
+    content: "This weapon adds 5 feet to your reach when you attack with it."
+  })
+})
+
+$(function () {
+  $('.special-property').popover({
+    title: "Special Property",
+    content: "A weapon with the special property has unusual rules governing its use, explained in the weapon's description."
+  })
+})
+
+$(function () {
+  $('.thrown-property').popover({
+    title: "Thrown Property",
+    content: "If a weapon has the thrown property, you can throw the weapon to make a ranged attack. If the weapon is a melee weapon, you use the same ability modifier for that attack roll and damage roll that you would use for a melee attack with the weapon. For example, if you throw a handaxe, you use your Strength, but if you throw a dagger, you can use either your Strength or your Dexterity, since the dagger has the finesse property."
+  })
+})
+
+$(function () {
+  $('.two-handed-property').popover({
+    title: "Two-Handed Property",
+    content: "This weapon requires two hands to use."
+  })
+})
+
+$(function () {
+  $('.versatile-property').popover({
+    title: "Versatile Property",
+    content: "This weapon can be used with one or two hands. A damage value in parentheses appears with the property—the damage when the weapon is used with two hands to make a melee attack."
+  })
+})
 
 var csrftoken = getCookie('csrftoken');
 
